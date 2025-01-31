6e9d6ccb9b1537e8c1cf0032f1ea44d05fc90c38
0xCrock <jakecrocker83@gmail.com>
2025-01-31 10:03:58 -0600

    Remove docs link.

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,6 @@ const App = () => {
       )}
 
       {showPublicKeyInvalidMessage ? <PleaseSetYourPublicKeyMessage /> : null}
-      <ReturnToDocsLink />
     </div>
   );
 };
@@ -126,27 +125,3 @@ const PleaseSetYourPublicKeyMessage = () => {
     </div>
   );
 };
-
-const ReturnToDocsLink = () => {
-  return (
-    <a
-      href="https://docs.vapi.ai"
-      target="_blank"
-      rel="noopener noreferrer"
-      style={{
-        position: "fixed",
-        top: "25px",
-        right: "25px",
-        padding: "5px 10px",
-        color: "#fff",
-        textDecoration: "none",
-        borderRadius: "5px",
-        boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
-      }}
-    >
-      return to docs
-    </a>
-  );
-};
-
-export default App;
